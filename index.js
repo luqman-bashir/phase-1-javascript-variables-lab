8e710b06bf85af6aac1e0fb31c1f62931d166b54
luqman-bashir <uthamin98@gmail.com>
2024-09-26 11:55:26 +0300

    variable-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+// const companyName=;
+// // Code your solution in this file!  
+
+// 1. Declare companyName as a constant and set it to "Scuber"  
+ companyName = 'Scuber';  
+
+// 2. Declare mostProfitableNeighborhood with a let declaration  
+let mostProfitableNeighborhood = 'Chelsea'; // Replace with the actual value needed  
+
+// 3. Declare companyCeo with a let declaration  
+let companyCeo = 'Susan Smith'; // Replace with the actual CEO name as needed
